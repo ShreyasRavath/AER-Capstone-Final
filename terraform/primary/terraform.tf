dab44e98fe3fd6a42a6288094849491555287c1a
ShreyasRavath <34619797+ShreyasRavath@users.noreply.github.com>
2025-07-21 15:13:19 +0530

    added files

---
+++ terraform/primary/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 5.1.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "aer-finalproject"
+    key          = "aer-finalproject-dr.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
